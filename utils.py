0395ad3b6cb6c114af14bb644bb5c39d34325623
DCI-xurxo <xurxo.astorgano@dci-student.org>
2025-09-15 11:20:09 +0200

    first workflow

---
+++ utils.py
@@ -0,0 +1,10 @@
+def porcentage_calculator(percent, whole):
+    if whole == 0:
+        return 0
+    else:
+        return (percent/100) * whole
+    
+def is_palindrome(s)
+    word = s.replace(" ","")
+    word = word.lower()
+    word = word.lower()
