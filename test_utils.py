335b0b118473e02360df4cc4dca7b3407e376f5f
DCI-xurxo <xurxo.astorgano@dci-student.org>
2025-09-15 11:45:24 +0200

    fourth workflow

--- test_utils.py
+++ test_utils.py
@@ -1,9 +1,9 @@
 from utils import percentage_calculator, is_palindrome
 
 def test_percentage_calculator():
-    assert test_percentage_calculator(20,10) == 2
-    assert test_percentage_calculator(0,100) == 0
-    assert test_percentage_calculator(5,0) == 0
+    assert percentage_calculator(20,10) == 2
+    assert percentage_calculator(0,100) == 0
+    assert percentage_calculator(5,0) == 0
 
     def test_is_paliandrome():
         assert is_palindrome("madam")
